92d90de672789cc5721a1cb8c8e51861bc044ece
GuoPei <guopei@Boluomi.local>
2015-05-13 11:33:03 +0800

    Initial Commit

---
+++ LibLinearObj/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LibLinearObj
+//
+//  Created by GuoPei on 15/5/13.
+//  Copyright (c) 2015年 GuoPei. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
