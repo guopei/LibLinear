7a8d516f6d29e0aadb1e70430312c372aaf8abd0
GuoPei <guopei@Boluomi.local>
2015-05-14 11:11:13 +0800

    Add more solver type choice.

--- LibLinearObj/LibLinear.hpp
+++ LibLinearObj/LibLinear.hpp
@@ -5,12 +5,21 @@ using namespace std;
 using namespace cv;
 
 #define Malloc(type,n) (type *)malloc((n)*sizeof(type))
+#define INF HUGE_VAL
 
 class LinearParam{
+
 public:
+    /**
+     * Default solver 1.
+     */
 	static parameter construct_param(){
 		return parameter{ L2R_L2LOSS_SVC_DUAL, 0.1, 1, 0, NULL, NULL, 0.1 };
 	}
+    /*
+     * User-defined parameters. Use this only when you know
+     * what you are doing.
+     */
 	static parameter construct_param(int solver_type,
 		double eps,
 		double C,
@@ -20,10 +29,101 @@ public:
 		double p){
 		return parameter{ solver_type, eps, C, nr_weight, weight_label, weight, p };
 	}
+    
+    /*
+     * Only choose solver type and the rest is set for you.
+     * Recommand: choose solver 1 first. If warning appears,
+     * choose solver 2. If you want to have probability output, 
+     * choose solver 0 or 7.
+     */
+    static parameter construct_param(int solver_type){
+        double C = 1;
+        double p = 0.1;
+        double eps = INF;
+        int nr_weight = 0;
+        int *weight_label = nullptr;
+        double* weight = nullptr;
+        
+        if(solver_type > L2R_LR_DUAL || solver_type < L2R_LR)
+            exit_with_help();
+        
+        switch (solver_type)
+        {
+            case L2R_LR:
+            case L2R_L2LOSS_SVC:
+                eps = 0.01;
+                break;
+            case L2R_L2LOSS_SVR:
+                eps = 0.001;
+                break;
+            case L2R_L2LOSS_SVC_DUAL:
+            case L2R_L1LOSS_SVC_DUAL:
+            case MCSVM_CS:
+            case L2R_LR_DUAL:
+                eps = 0.1;
+                break;
+            case L1R_L2LOSS_SVC:
+            case L1R_LR:
+                eps = 0.01;
+                break;
+            case L2R_L1LOSS_SVR_DUAL:
+            case L2R_L2LOSS_SVR_DUAL:
+                eps = 0.1;
+                break;
+        }
+        
+        return parameter{ solver_type, eps, C, nr_weight, weight_label, weight, p };
+    }
+    
+    static void exit_with_help()
+    {
+        printf(
+               "Usage: train [options] training_set_file [model_file]\n"
+               "options:\n"
+               "-s type : set type of solver (default 1)\n"
+               "  for multi-class classification\n"
+               "	 0 -- L2-regularized logistic regression (primal)\n"
+               "	 1 -- L2-regularized L2-loss support vector classification (dual)\n"
+               "	 2 -- L2-regularized L2-loss support vector classification (primal)\n"
+               "	 3 -- L2-regularized L1-loss support vector classification (dual)\n"
+               "	 4 -- support vector classification by Crammer and Singer\n"
+               "	 5 -- L1-regularized L2-loss support vector classification\n"
+               "	 6 -- L1-regularized logistic regression\n"
+               "	 7 -- L2-regularized logistic regression (dual)\n"
+               "  for regression\n"
+               "	11 -- L2-regularized L2-loss support vector regression (primal)\n"
+               "	12 -- L2-regularized L2-loss support vector regression (dual)\n"
+               "	13 -- L2-regularized L1-loss support vector regression (dual)\n"
+               "-c cost : set the parameter C (default 1)\n"
+               "-p epsilon : set the epsilon in loss function of SVR (default 0.1)\n"
+               "-e epsilon : set tolerance of termination criterion\n"
+               "	-s 0 and 2\n"
+               "		|f'(w)|_2 <= eps*min(pos,neg)/l*|f'(w0)|_2,\n"
+               "		where f is the primal function and pos/neg are # of\n"
+               "		positive/negative data (default 0.01)\n"
+               "	-s 11\n"
+               "		|f'(w)|_2 <= eps*|f'(w0)|_2 (default 0.001)\n"
+               "	-s 1, 3, 4, and 7\n"
+               "		Dual maximal violation <= eps; similar to libsvm (default 0.1)\n"
+               "	-s 5 and 6\n"
+               "		|f'(w)|_1 <= eps*min(pos,neg)/l*|f'(w0)|_1,\n"
+               "		where f is the primal function (default 0.01)\n"
+               "	-s 12 and 13\n"
+               "		|f'(alpha)|_1 <= eps |f'(alpha0)|,\n"
+               "		where f is the dual function (default 0.1)\n"
+               "-B bias : if bias >= 0, instance x becomes [x; bias]; if < 0, no bias term added (default -1)\n"
+               "-wi weight: weights adjust the parameter C of different classes (see README for details)\n"
+               "-v n: n-fold cross validation mode\n"
+               "-q : quiet mode (no outputs)\n"
+               );
+        exit(1);
+    }
+
 };
 
 
 class LibLinear{
+    
 private:
 	struct parameter _param;
 	struct problem _prob;
@@ -57,6 +157,7 @@ private:
 		for (int i = 0; i < LabelMat.rows; i++)
 			prob.y[i] = LabelMat.at<float>(i);
 	}
+    
 	/**
 	 * one sample each call.
 	 */
@@ -71,6 +172,7 @@ private:
 	}
 
 public:
+    
 	~LibLinear(){
 		destroy_param(&_param);
         if(_model)
@@ -82,15 +184,18 @@ public:
 		if(_sample)
             free(_sample);
 	}
+    
 	LibLinear(){
 		_param = parameter();
 		_prob = problem();
 		_model = nullptr;
 		_sample = nullptr;
 	}
+    
 	void save_model(string filename){
         ::save_model(filename.c_str(), _model);
 	}
+    
 	/**
 	 * FeatureMat: M samples * N features.
 	 * LabelMat: N lables * 1.
@@ -146,6 +251,7 @@ public:
 			free(x);
 		}
 	}
+    
     void load_model(string model_file_name){
         if(_model){
             free_and_destroy_model(&_model);
